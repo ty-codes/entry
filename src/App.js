333dc2d1475d3e17b7dba5f8da1a95d16decd46d
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-03-27 03:15:06 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Suspense} from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <>
+
+      <Router>
+        <div>
+          <Routes>
+          </Routes>
+
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
