7c663fc67ec2649ddd73c5014ab9be2566934090
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-03-27 03:15:31 -0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
+import './index.css';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+serviceWorkerRegistration.unregister();
+
+reportWebVitals();
