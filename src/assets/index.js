4f96cfdc61224f7dc840db4a62b1b0159faa517c
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-03-29 09:38:07 -0800

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import logo from "./logo.png";
+import hero from "./hero.png";
+import sliderShapeLong from "./slider-shape-1.png";
+import sliderShapeCurve from "./slider-shape-5.png";
+
+export {
+    logo,
+    hero,
+    sliderShapeCurve,
+    sliderShapeLong
+}
