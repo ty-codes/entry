7cd029af197891e658ff93fc88b40a4032e96e9b
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-03-30 10:10:19 -0800

    Add files via upload

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,20 @@ import logo from "./logo.png";
 import hero from "./hero.png";
 import sliderShapeLong from "./slider-shape-1.png";
 import sliderShapeCurve from "./slider-shape-5.png";
+import sliderShapeYellow from "./slider-shape-2.png";
+import postProject from "./post-project.png";
+import jobsAndInternships from "./jobs-internships.png";
+import firstOpportunity from "./first-opportunity.png";
+
+
 
 export {
     logo,
     hero,
     sliderShapeCurve,
-    sliderShapeLong
+    sliderShapeLong,
+    sliderShapeYellow,
+    postProject,
+    jobsAndInternships,
+    firstOpportunity
 }
